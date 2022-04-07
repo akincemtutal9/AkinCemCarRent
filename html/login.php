8a752ddf7ebcf477b98c44fbe754b01f59a0f37c
akincemtutal9 <akintutall@gmail.com>
2022-04-07 20:27:23 +0300

    Some changes
    background images for better visual , added background photo

--- html/login.php
+++ html/login.php
@@ -10,6 +10,12 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
   <title>Login</title>
+  <style>
+        body{
+            background-image: url(../images/Photos/car\ for\ background.jpg);
+        }
+    </style>
+
 </head>
 <body>
   
